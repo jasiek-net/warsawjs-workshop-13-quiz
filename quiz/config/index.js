12eb02f17b2843bf2ee2f5c46211ef142dfd2bc5
Jan Horubała <jan.horubala@gmail.com>
2017-12-11 18:36:54 +0100

    Added publishing script

--- quiz/config/index.js
+++ quiz/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/warsawjs-workshop-13-quiz',
 
     /**
      * Source Maps
